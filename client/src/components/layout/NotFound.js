b6e18ab0aee5682816138c82ec8022b8d134eb5a
Darryl Karney <dkarney@outlook.com>
2022-02-11 18:47:05 -0600

    Correct issue where login button was not working on login page

--- client/src/components/layout/NotFound.js
+++ client/src/components/layout/NotFound.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const NotFound = () => {
   return (
-    <section className='container'>
-      <h1 className='x-large text-primary'>
-        <i className='fas fa-exclamation-triangle' /> Page Not Found
+    <section className="container">
+      <h1 className="x-large text-primary">
+        <i className="fas fa-exclamation-triangle" /> Page Not Found
       </h1>
-      <p className='large'>Sorry, this page does not exist</p>
+      <p className="large">Sorry, this page does not exist</p>
     </section>
   );
 };
