8ace508814be458d513370f51bf95db86abf4028
Darryl Karney <dkarney@outlook.com>
2022-03-05 09:24:20 -0600

    Remove gravatar display on profile page. Remove extra padding for profile header in css.

--- client/src/components/profile/ProfileTop.js
+++ client/src/components/profile/ProfileTop.js
@@ -8,7 +8,6 @@ const ProfileTop = ({
 }) => {
   return (
     <div className="profile-top bg-primary p-2">
-      <img className="round-img my-1" src={avatar} alt="" />
       <h1 className="large">{name}</h1>
     </div>
   );
