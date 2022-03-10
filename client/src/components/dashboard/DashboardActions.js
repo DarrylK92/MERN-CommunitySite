63c57b945ababcc834ce17766af91c7ffab774a7
Darryl Karney <dkarney@outlook.com>
2022-03-10 15:44:42 -0600

    Only display add event button if user is an organization

--- client/src/components/dashboard/DashboardActions.js
+++ client/src/components/dashboard/DashboardActions.js
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { loadUser } from '../../actions/auth';
 
-const DashboardActions = () => {
+const DashboardActions = ({ user }) => {
+  useEffect(() => {
+    if (!user) {
+      loadUser();
+    }
+  }, [loadUser]);
+
+  let extraButtonsDisplay;
+
+  if (user !== null) {
+    if (user.type === 'Organizer') {
+      extraButtonsDisplay = (
+        <Link to="/add-event" className="btn btn-light">
+          <i className="fas fa-plus-square text-primary" /> Add Event
+        </Link>
+      );
+    }
+  }
+
   return (
     <div className="dash-buttons">
       <Link to="/edit-profile" className="btn btn-light">
         <i className="fas fa-user-circle text-primary" /> Edit Profile
       </Link>
-      <Link to="/add-event" className="btn btn-light">
-        <i className="fas fa-plus-square text-primary" /> Add Event
-      </Link>
+      {extraButtonsDisplay}
     </div>
   );
 };
 
-export default DashboardActions;
+DashboardActions.propTypes = {
+  user: PropTypes.object.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  user: state.auth.user
+});
+
+export default connect(mapStateToProps)(DashboardActions);
